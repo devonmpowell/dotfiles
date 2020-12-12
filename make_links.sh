b9a63cce8e3c6bf558e9cf1425886b12b7f98a5c
Devon Powell <dmpowell@mpa-garching.mpg.de>
2020-12-12 14:20:50 +0100

    proper directory checks

--- make_links.sh
+++ make_links.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm $HOME/.bashrc $HOME/.vimrc $HOME/.vim $HOME/.screenrc $HOME/.ssh/config
-#ln -s $DIR/profile $HOME/.profile  
+mkdir -p $HOME/.matplotlib
+mkdir -p $HOME/.ssh
+rm $HOME/.bashrc $HOME/.vimrc $HOME/.vim $HOME/.screenrc $HOME/.matplotlib/matplotlibrc $HOME/.ssh/config
 ln -s $DIR/bashrc $HOME/.bashrc  
 ln -s $DIR/vimrc $HOME/.vimrc
 ln -s $DIR/vim $HOME/.vim
 ln -s $DIR/screenrc $HOME/.screenrc
-mkdir -p $HOME/.matplotlib
 ln -s $DIR/matplotlibrc $HOME/.matplotlib/matplotlibrc
 ln -s $DIR/ssh_config $HOME/.ssh/config
-source $HOME/.bashrc $HOME/.vimrc $HOME/.vim $HOME/.screenrc
-
+source $HOME/.bashrc $HOME/.vimrc $HOME/.vim $HOME/.screenrc $HOME/.matplotlib/matplotlibrc $HOME/.ssh/config
